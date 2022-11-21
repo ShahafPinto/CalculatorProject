a8a6c0e53803716159d5ae787f9b2b217b4f80d2
ShahafPinto <shahafchen7@gmail.com>
2022-11-21 14:07:26 +0200

    ..

--- JS/application.js
+++ JS/application.js
@@ -1,19 +1,19 @@
-document.getElementById("info").onclick = function() {infoFunction()};
 
-
-function infoFunction() {
-    alert('Developer\'s name: Shahaf Pinto\nCalculator\'s version (stage): 1\nDescription: This Calculator is a device that performs arithmetic operations on numbers.\nBasic calculators can do only addition, subtraction, multiplication and division mathematical calculations.');
-}
-
-document.getElementById("light").onclick = function() {lightstatefunc()};
-document.getElementById("history").onclick = function() {historystatefunc()};
-document.getElementById("scientific").onclick = function() {scientificstatefunc()};
-document.getElementById("remote").onclick = function() {remotestatefunc()};
+document.getElementById("info").addEventListener("click", infoFunction);
+document.getElementById("light").addEventListener("click", lightstatefunc);
+document.getElementById("history").addEventListener("click", historystatefunc);
+document.getElementById("scientific").addEventListener("click", scientificstatefunc);
+document.getElementById("remote").addEventListener("click", remotestatefunc); 
 
 let lightstate = 'True';
 let historystate = 'True';
 let scientificstate = 'True';
 let remotestate = 'True';
+
+function infoFunction() {
+    alert('Developer\'s name: Shahaf Pinto\nCalculator\'s version (stage): 1\nDescription: This Calculator is a device that performs arithmetic operations on numbers.\nBasic calculators can do only addition, subtraction, multiplication and division mathematical calculations.');
+}
+
 function lightstatefunc(){
     lightstate = !lightstate
     alert(lightstate)
